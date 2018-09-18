191d8422b07180508b37299460de60e1b8fafc28
Chan Yeung Yeh <brian@localhost.localdomain>
2018-09-18 10:22:44 +0800

    First commit

---
+++ pe.py
@@ -0,0 +1,21 @@
+import binascii
+import codecs
+import binhex
+#def writefile():
+	#with open("")
+
+def readfile(res_f): 
+	with open("0A32eTdBKayjCWhZqDOQ.bytes", 'r') as init_file:
+		for f in init_file:
+    			#data = f.readline()
+			arr = f.split()
+			for i in range(len(arr)):
+				if(i != 0):
+					res_f.write("%s" % binascii.b2a_hex(arr[i]))
+			res_f.write("\n")
+	return res_f
+
+if __name__ == "__main__":
+      file = open("result.txt", 'w+')
+      file = readfile(file)
+      file.close()
